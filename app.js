3d74918672e893ef0f198c20bf4428bcd6c747c8
8309h <harshalwagh201718@gmail.com>
2024-04-27 20:32:38 +0530

    delpoy success

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-app.get(`/${baseURL}`, (req, res) => {
+app.get(`/${baseURL}/`, (req, res) => {
   res.send('Wel-come to the Book Store!');
 });
 
